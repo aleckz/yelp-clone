9fdbf54c3aa423a4ad257aa9b480e047516e3ff8
jonathansayer <jon.sayer1@gmail.com>
2015-07-21 16:35:36 +0100

    deleting restaurant, deletes associated reviews

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -14,3 +14,23 @@ feature 'reviewing' do
     expect(page).to have_content('so so')
   end
 end
+
+feature 'reviews' do
+
+  scenario 'are destroyed when the associated retaurant is destroyed' do 
+    Restaurant.create name:'KFC'
+    delete_restaurant
+    expect(page).not_to have_content('so so')
+  end 
+
+  def delete_restaurant
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in "Thoughts", with: "so so"
+    select '3', from: 'Rating'
+    click_button 'Leave Review'
+    visit '/restaurants'
+    click_link 'Delete KFC'
+  end
+
+end 
---
+++ spec/models/review_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe Review, type: :model do
+  it { is_expected.to belong_to :restaurant}
+
+  it 'should be destroyed when restaurant is destroyed' do
+    kfc = Restaurant.create name:'KFC'
+    kfc.reviews.create(thoughts: 'so so')
+    kfc.destroy 
+    expect(Review.find_by thoughts: 'so so').to equal nil
+  end
+end
