ae26f9b9f929a80ab37978c8817ffea900b501d4
Alex <aleksandernasr@gmail.com>
2015-07-22 15:40:19 +0100

    User authentication, omniauth, hidden env app_secrets, facebook login complete

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -115,7 +115,7 @@ Devise.setup do |config|
   # their account can't be confirmed with the token any more.
   # Default is nil, meaning there is no restriction on how long a user can take
   # before confirming their account.
-  # config.confirm_within = 3.days
+  config.confirm_within = 3.days
 
   # If true, requires any email changes to be confirmed (exactly the same way as
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
@@ -239,6 +239,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, info_fields: "email"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
